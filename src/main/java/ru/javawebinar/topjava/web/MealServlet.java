ebe1feed0b456fadb8232e49c1977bd8b730fd7b
Vpp2003 <Vpp2003@gmail.com>
2023-02-09 21:08:01 +0200

    lesson 1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,24 +1,45 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealStorage;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 
 public class MealServlet extends HttpServlet {
+    private MealStorage mealStorage = new MealStorage();
     private static final Logger log = getLogger(MealServlet.class);
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Map<LocalDate, Integer> caloriesSumByDate = mealStorage.getMeals().stream()
+                .collect(
+                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
+                );
+
+        List<MealTo> mealToList = mealStorage.getMeals().stream()
+                .map(meal -> new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), caloriesSumByDate.get(meal.getDate()) > MealStorage.CALORIESPERDAY))
+                .collect(Collectors.toList());
+
+        request.setAttribute("mealToList", mealToList);
+
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        //  response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -6,11 +6,38 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="ru">
 <head>
-    <title>Title</title>
+    <title>Meals</title>
 </head>
 <body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+<table border="2">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th></th>
+        <th></th>
+    </tr>
 
+    <c:forEach var="mealTo" items="${mealToList}">
+        <c:choose>
+            <c:when test="${mealTo.excess}">
+                <tr style="color: red">
+            </c:when>
+            <c:otherwise>
+                <tr style="color: green">
+            </c:otherwise>
+        </c:choose>
+        <td>${mealTo.dateTime}</td>
+        <td>${mealTo.description}</td>
+        <td>${mealTo.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
