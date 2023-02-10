63bf0dfb6d4055bb1160bd9cf22acdf2d2d4a2f0
Vpp2003 <Vpp2003@gmail.com>
2023-02-10 21:02:16 +0200

    lesson 1

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -60,6 +60,7 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+
         return repository.values().stream()
                 .filter(user -> user.getEmail().equals(email))
                 .findFirst()
